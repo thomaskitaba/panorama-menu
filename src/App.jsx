5000c6d9e6e7c4359de87f877b18863ed0a56ed9
thomaskitaba <thomas.kitaba@gmail.com>
2024-06-26 02:40:47 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import MyContextProvider from './components/MyContextProvider';
+
+import './App.css';
+import Heropage from './components/Heropage';
+import Holidays from './components/Holidays';
+import Messageoftheday from './components/Messageoftheday';
+import Menu from './components/Menu';
+import Catagory from './components/Catagory';
+import Welcome from './components/Welcome';
+import Navigation from './components/MegaMenuNavbar';
+import MegaMenuNavbar from './components/MegaMenuNavbar';
+
+function App() {
+
+  return (
+    <>
+      <div className="app-container">
+        <MyContextProvider>
+          <MegaMenuNavbar />
+          <Heropage />
+          {/* <Holidays /> */}
+          <Messageoftheday />
+          <Menu />
+          <Welcome />
+        </MyContextProvider>
+      </div>
+    </>
+  )
+}
+
+export default App;
