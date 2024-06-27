c6349637363d7c2d88466e6be7b2ea18c00b8458
thomaskitaba <thomas.kitaba@gmail.com>
2024-06-27 13:26:16 +0300

    update

--- src/components/Packagejson.jsx
+++ src/components/Packagejson.jsx
@@ -5,8 +5,8 @@ const Packagejson = [{
   "package-name": "Diaspora",
   "package-price": "starting from $40",
   "package-description": "for diaspora and anyone intrested",
-  "package-startdate": "2021-09-01",
-  "package-enddate": "2021-09-30",
+  "package-startdate": "2024-09-01",
+  "package-enddate": "2024-09-30",
   "package-availability": "available",
   "package-image": "https://images.unsplash.com/photo-1607456094469-2e908b07759f"
 },
@@ -14,8 +14,8 @@ const Packagejson = [{
   "package-name": "Romantic Escape",
   "package-price": "1500",
   "package-description": "Perfect getaway for couples with special romantic touches.",
-  "package-startdate": "2022-02-01",
-  "package-enddate": "2022-02-28",
+  "package-startdate": "2024-02-01",
+  "package-enddate": "2024-02-28",
   "package-availability": "available",
   "package-image": "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0"
 },
@@ -23,8 +23,8 @@ const Packagejson = [{
   "package-name": "Family Fun",
   "package-price": "2000",
   "package-description": "An all-inclusive package designed for families, featuring activities for all ages.",
-  "package-startdate": "2022-06-01",
-  "package-enddate": "2022-06-30",
+  "package-startdate": "2024-06-01",
+  "package-enddate": "2024-06-30",
   "package-availability": "not-available",
   "package-image": "https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445"
 },
@@ -32,8 +32,8 @@ const Packagejson = [{
   "package-name": "Adventure Seeker",
   "package-price": "1800",
   "package-description": "Thrill-packed package for adventure enthusiasts with outdoor activities.",
-  "package-startdate": "2022-09-01",
-  "package-enddate": "2022-09-30",
+  "package-startdate": "2024-09-01",
+  "package-enddate": "2024-09-30",
   "package-availability": "available",
   "package-image": "https://images.unsplash.com/photo-1504851149312-7a075b496cc7"
 }];
